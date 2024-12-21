da770949a21dbec646312fea3f6d217c5013ea30
Jeffery Wu <jefferyl.wu@gmail.com>
2024-12-21 06:40:33 -0500

    Updated site with information and new features.

--- build/main.js
+++ build/main.js
@@ -3,19 +3,58 @@ const initApp = () => {
     const mobileMenu = document.getElementById("mobile-menu");
     const mobileMenuButtons = mobileMenu.children;
 
-    const toggleMenu = () =>{
+    const projects = document.getElementsByClassName("project");
+    const popup = document.getElementById("popup");
+    const popText = document.getElementById("popText");
+    const popTitle = document.getElementById("popTitle");
+    const closePopup = document.getElementById("closePopup");
+
+
+    const closePop = (e) => {
+        if (popup) {
+            popup.style.visibility = "hidden";
+            document.body.style.pointerEvents = "all";
+            for (let i = 0; i < projects.length; i++) {
+                projects.item(i).style.pointerEvents = "all";
+            }
+            document.body.style.overflow = "visible";
+
+        }
+    }
+    const openPop = (e) => {
+        if (popup) {
+            for (let i = 0; i < projects.length; i++) {
+                projects.item(i).style.pointerEvents = "none";
+            }
+
+            popTitle.textContent = e.target.closest(".project").querySelector(".title").textContent;
+            popText.textContent = e.target.closest(".project").querySelector(".description").textContent;
+            popup.style.visibility = "visible";
+            document.body.style.pointerEvents = "none";
+            document.body.style.overflow = "hidden";
+        }
+
+    }
+
+    closePopup.addEventListener('click', closePop);
+    for (let i = 0; i < projects.length; i++) {
+        projects.item(i).addEventListener('click', openPop);
+    }
+
+
+    const toggleMenu = () => {
         mobileMenu.classList.toggle("hidden");
         mobileMenu.classList.toggle("flex");
         hamburgerMenu.classList.toggle("toggle-menu");
     }
 
     hamburgerMenu.addEventListener('click', toggleMenu);
-    for (let i = 0; i < mobileMenuButtons.length; i++){
+    for (let i = 0; i < mobileMenuButtons.length; i++) {
         mobileMenuButtons.item(i).addEventListener('click', toggleMenu);
     }
-    
 
-    
+
+
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
